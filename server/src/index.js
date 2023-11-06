de571fd294e9898130f0f3c026524a298a557a73
Atishay <hritikjain707@gmail.com>
2023-11-06 20:56:53 +0530

    Init commit

---
+++ server/src/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`server running at PORT:${PORT}`);
+});
