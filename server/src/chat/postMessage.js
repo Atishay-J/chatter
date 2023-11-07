7755ac74ff3eb6630605f251539378809930669d
Atishay <hritikjain707@gmail.com>
2023-11-07 20:35:22 +0530

    Fix: Add emit type for group msg

--- server/src/chat/postMessage.js
+++ server/src/chat/postMessage.js
@@ -5,7 +5,7 @@ const postMessage = (server_history, socket, msgObj, userName, roomName) => {
   if (!isValidMsg) {
     return socket.in(roomName).to(userName).emit('invalid Msg');
   }
-  socket.in(roomName).emit(msgObj);
+  socket.in(roomName).emit('new message', msgObj);
   const room_history = server_history?.[roomName] || {};
   const updatedRoomHistory = addMsgToRoomHistory(
     room_history,
